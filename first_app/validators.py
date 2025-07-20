5076e951eae8558a944e8e32e082556f12ba893e
wasabi52ngg <vovasharafutdinov@mail.ru>
2025-07-20 10:35:02 +0300

    Added new validator

--- first_app/validators.py
+++ first_app/validators.py
@@ -7,3 +7,9 @@ def valid_email(mail):
     mail_regex = re.compile(r"^\w+@[a-zA-Z]+.[a-zA-Z]{2,}$")
     if not mail_regex.match(mail):
         raise ValidationError("Неверный формат почты")
+
+
+def valid_phone(phone):
+    phone_regex = re.compile(r'^\+\d-\d{3}-\d{3}-\d{2}-\d{2}$')
+    if not phone_regex.match(phone):
+        raise ValidationError("Неверный формат телефона")
